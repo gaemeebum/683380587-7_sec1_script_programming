86f5aa063c07f64d9b3ef4cf74a794522ce99cb3
gummeebear <sasiwan.chu@kkumail.com>
2026-07-05 20:24:10 +0700

    lab2

--- week 2/lab2_2.py
+++ week 2/lab2_2.py
@@ -11,5 +11,5 @@ else:
 
     like = input("Do you like action movies? (yes/no): ").strip().lower()
 
-    if like == "yes":
+    if like == "ys":
         print("You might enjoy the latest action blockbuster!")
