e329906f520f04845f0e72c8e0cbd1fa149df24f
gummeebear <sasiwan.chu@kkumail.com>
2026-07-05 20:55:48 +0700

    lab2

--- week 2/lab2_2.py
+++ week 2/lab2_2.py
@@ -3,9 +3,9 @@ age = int(input("Enter your age: "))
 if age < 5:
     print("You're too young for movies! Enjoy cartoons.")
 elif age <= 12:
-    print("You can watch G-rated movies.")
+    print("You can watch G-rated or or PG-rated movies.")
 elif age <= 17:
-    print("You can watch PG-13 movies.")
+    print("You can watch PG-13 or R-rated (with parental guidance).")
 else:
     print("You can watch any movie rating.")
 
